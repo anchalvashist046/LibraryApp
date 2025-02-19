83752cf61ad70223d3e91c59b702e4cd10e7af37
Egypta Gogoi <egyptagogoi@Egyptas-MacBook-Pro.local>
2025-02-19 16:25:36 +0530

    updated errors in app.py

--- app.py
+++ app.py
@@ -1,25 +1,10 @@
-# from app import create_app, db
-#
-# app = create_app()
-#
-# if __name__ == '__main__':
-#     with app.app_context():
-#         db.create_all()  # Create tables if they don't exist
-#     app.run(debug=True)
+
 
 from flask import Flask
-import sqlalchemy
-from sqlalchemy.orm import sessionmaker
-import config
+from LibraryApp import create_app  # Import the create_app function
 
+app = create_app()  # Create the Flask app instance
 
-app = Flask(__name__)
-app.config.from_object(config)
-
-
-# Database setup
-# engine = sqlalchemy.create_engine(config.SQLALCHEMY_DATABASE_URI)
-# SessionLocal = sessionmaker(bind=engine)
 
 @app.route("/")
 def home():
